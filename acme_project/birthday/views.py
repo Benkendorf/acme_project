1a4fb05ad3cd79fc9597ceb6f85701e416c4e288
Egor Korotaev <ego_kor@mail.ru>
2024-10-26 18:03:43 +0300

    After Sprint 10/19 Topic 2/4 Lesson 2/4

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
@@ -11,9 +12,28 @@ from django.views.generic import (
 from django.urls import reverse_lazy
 
 # Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
-from .forms import BirthdayForm
-from .models import Birthday
+from .forms import BirthdayForm, CongratulationForm
+from .models import Birthday, Congratulation
 from .utils import calculate_birthday_countdown
+
+
+@login_required
+def add_comment(request, pk):
+    # Получаем объект дня рождения или выбрасываем 404 ошибку.
+    birthday = get_object_or_404(Birthday, pk=pk)
+    # Функция должна обрабатывать только POST-запросы.
+    form = CongratulationForm(request.POST)
+    if form.is_valid():
+        # Создаём объект поздравления, но не сохраняем его в БД.
+        congratulation = form.save(commit=False)
+        # В поле author передаём объект автора поздравления.
+        congratulation.author = request.user
+        # В поле birthday передаём объект дня рождения.
+        congratulation.birthday = birthday
+        # Сохраняем объект в БД.
+        congratulation.save()
+    # Перенаправляем пользователя назад, на страницу дня рождения.
+    return redirect('birthday:detail', pk=pk)
 
 
 class OnlyAuthorMixin(UserPassesTestMixin):
@@ -52,6 +72,7 @@ class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
 
 class BirthdayDetailView(DetailView):
     model = Birthday
+
     def get_context_data(self, **kwargs):
         # Получаем словарь контекста:
         context = super().get_context_data(**kwargs)
@@ -59,6 +80,14 @@ class BirthdayDetailView(DetailView):
         context['birthday_countdown'] = calculate_birthday_countdown(
             # Дату рождения берём из объекта в словаре context:
             self.object.birthday
+        )
+
+        context['form'] = CongratulationForm()
+        # Запрашиваем все поздравления для выбранного дня рождения.
+        context['congratulations'] = (
+            # Дополнительно подгружаем авторов комментариев,
+            # чтобы избежать множества запросов к БД.
+            self.object.congratulations.select_related('author')
         )
         # Возвращаем словарь контекста.
         return context
